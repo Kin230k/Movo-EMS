5be512d4c90b65a2c72bc6993b1009e386f41531
Wael Aljajeh <Wael123FF@gmail.com>
2025-07-24 00:59:14 +0300

    Remove unwanted files

--- sql_procedures/user_tables/delete_user_table.sql
+++
@@ -1,7 +0,0 @@
-CREATE OR REPLACE PROCEDURE delete_user_table_entry(p_user_id INT)
-LANGUAGE plpgsql AS $$
-BEGIN
-    DELETE FROM USER_TABLE
-    WHERE userId = p_user_id;
-END;
-$$;
--- sql_procedures/user_tables/get_all_user_tables.sql
+++
@@ -1,9 +0,0 @@
-CREATE OR REPLACE FUNCTION get_all_user_table_entries()
-RETURNS TABLE (userId INT, username VARCHAR(100))
-LANGUAGE plpgsql AS $$
-BEGIN
-    RETURN QUERY 
-    SELECT ut.userId, ut.username
-    FROM USER_TABLE ut;
-END;
-$$;
