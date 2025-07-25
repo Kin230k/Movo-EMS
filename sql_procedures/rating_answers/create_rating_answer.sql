7d2df4e8597e509c58ff575fbd54a49f7d38bb7f
Wael Aljajeh <waelaljajeh562@gmail.com>
2025-07-25 21:30:12 +0300

    Update create_rating_answer.sql
    Signed-off-by: Wael Aljajeh <waelaljajeh562@gmail.com>

--- sql_procedures/rating_answers/create_rating_answer.sql
+++ sql_procedures/rating_answers/create_rating_answer.sql
@@ -5,10 +5,6 @@ CREATE OR REPLACE PROCEDURE create_rating_answer(
 LANGUAGE plpgsql
 AS $$
 BEGIN
-    -- Validate rating range first
-    IF p_rating NOT BETWEEN 1 AND 5 THEN
-        RAISE EXCEPTION 'Rating must be between 1 and 5 (got %)', p_rating;
-    END IF;
 
     -- Insert into RATING_ANSWERS
     INSERT INTO RATING_ANSWERS (
@@ -22,7 +18,5 @@ BEGIN
     EXCEPTION
         WHEN foreign_key_violation THEN
             RAISE EXCEPTION 'Invalid answerId: % does not exist in ANSWERS table', p_answerId;
-        WHEN check_violation THEN
-            RAISE EXCEPTION 'Rating value % violates check constraint (must be 1-5)', p_rating;
 END;
 $$;
